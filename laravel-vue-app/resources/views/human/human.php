a46243f39449dd8c6f01ba3576b8802c3155f4f8
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2020-10-03 19:25:02 +0900

    views_php_file

---
+++ laravel-vue-app/resources/views/human/human.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>冒険の書/僕たちの物語</title>
+  <style>
+    body { font-size: 16pt; color: #999; }
+    h1 { font-size: 120pt; text-align: right; color: #f6f6f6; margin: -50px 0 -100px; }
+  </style>
+</head>
+<body>
+    <h1>冒険の書</h1>
+    <p><?php echo $fst; ?></p>
+</body>
+</html>
