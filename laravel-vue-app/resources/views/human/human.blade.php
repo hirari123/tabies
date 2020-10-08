2f9744bae712cdeb0b21fcfdfcb0cb4f47405463
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2020-10-08 11:03:35 +0900

    human.blade.phpの修正

--- laravel-vue-app/resources/views/human/human.blade.php
+++ laravel-vue-app/resources/views/human/human.blade.php
@@ -20,22 +20,12 @@
     <table>
       @csrf
 
-      <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
-      @error('name')
-        <tr><th>ERROR</th><td>{{$message}}</td></tr>
-      @enderror
+      @if ($errors->has('msg'))
+      <tr><th>ERROR</th><td>{{$errors->first('msg')}}</td></tr>
+      @endif
+      <tr><th>Message: </th><td><input type="text" name="msg" value="{{old('msg')}}"></td></tr>
+      <tr><th></th><td><input type="submit" value="送信する"></td></tr>
 
-      <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
-      @error('mail')
-      <tr><th>ERROR</th><td>{{$message}}</td></tr>
-      @enderror
-
-      <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
-      @error('age')
-      <tr><th>ERROR</th><td>{{$message}}</td></tr>
-      @enderror
-
-      <tr><th></th><td><input type="submit" value="send"></td></tr>
     </table>
   </form>
   @endsection
