70d1a890cf2b263a9205cf482347090c9977c455
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2020-10-28 22:03:39 +0900

    personファイル内のblade追加

---
+++ laravel-vue-app/resources/views/person/human.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts', 'humanapp')
+
+@section('title', 'Person.human')
+
+@section('menubar')
+  @parent
+  インデックスページ
+@endsection
+
+@section('content')
+  <table>
+    <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+    @foreach($items as $item)
+      <tr>
+        <td>{{$item->name}}</td>
+        <td>{{$item->mail}}</td>
+        <td>{{$item->age}}</td>
+      </tr>
+    @endforeach
+  </table>
+@endsection
+
+@section('footer')
+copyright 2020 hirakara.
+@endsection
